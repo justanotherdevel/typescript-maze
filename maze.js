0a1afa444349a7b602de8defa4b80a22b07bcb5e
justanotherdevel <shashwatyashaswi@gmail.com>
2017-04-18 16:46:25 +0530

    Completed the backend interface. Needs to update few more stuff and implement Dijkstra to finish the backend

--- maze.js
+++ maze.js
@@ -1,10 +1,27 @@
+//Creating a class for storing maze.
 var Maze = (function () {
     function Maze(r, c) {
-        for (var i = 0; i < r; i++) {
-            for (var j = 0; j < c; j++) {
-                this.maze[i][j] = Math.floor(Math.random() * 10) + 1;
-            }
+        //Fill this with whatever values you find suitable for connections in maze.
+        this.connectionPos = [[1, 3], [3, 5]];
+        this.row = r;
+        this.column = c;
+        var noConnection = Math.floor(Math.random() * (this.maxConnection - this.minConnection)) + this.minConnection;
+        for (var i = 0; i < noConnection; i++) {
+            var pos = Math.floor(Math.random() * this.noPos);
+            this.maze[this.connectionPos[pos][0]][this.connectionPos[pos][1]] = Math.floor(Math.random() * 10) + 1;
         }
     }
+    Maze.prototype.extractWeight = function (i, j) {
+        return this.maze[i][j];
+    };
+    Maze.prototype.dijkstra = function (source, destination) {
+        //Apply dijkstra and check if destination is reachable.
+        //Return true or false accordingly.
+    };
     return Maze;
 }());
+var maze;
+maze = new Maze(50, 50);
+while (!maze.dijkstra(0, 0, 49, 49)) {
+    maze = new Maze(50, 50);
+}
--- maze.ts
+++ maze.ts
@@ -1,11 +1,36 @@
 //Creating a class for storing maze.
 class Maze {
-	protected maze:number[][]; 
+	//Update the maze to a pseudo dynamic one that will solve lots of
+	//problem that we'll face with visualization.
+	protected maze:number[][];
+	protected row:number;
+	protected column:number;
+	//Fill this with whatever values you find suitable for connections in maze.
+	protected connectionPos:number[][] = [[1,3],[3,5]];
+	protected noPos:number;
+	//Also update this with suitable range of connections.
+	private minConnection:number;
+	private maxConnection:number;
 	constructor (r:number, c:number) {
-		for (var i:number = 0; i < r; i++) {
-			for (var j:number = 0; j < c; j++) {
-				this.maze[i][j] = Math.floor(Math.random() * 10) + 1;
-			}
-		}	
+		this.row = r;
+		this.column = c;
+		let noConnection:number = Math.floor(Math.random()*(this.maxConnection - this.minConnection)) + this.minConnection;		
+		for (let i:number = 0; i < noConnection; i++) {
+			let pos:number = Math.floor(Math.random()*this.noPos);
+			this.maze[this.connectionPos[pos][0]][this.connectionPos[pos][1]] = Math.floor(Math.random()*10)+1;
+		}
 	}
+	public extractWeight (i:number, j:number) {
+		return this.maze[i][j]; 
+	}
+	public dijkstra (source:number, destination:number) {
+		//Apply dijkstra and check if destination is reachable.
+		//Return true or false accordingly.
+	}
+}
+
+let maze;
+maze = new Maze(50, 50);
+while (!maze.dijkstra(0, 0, 49, 49)) {
+	maze = new Maze(50, 50);		
 }
