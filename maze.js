7836309537f116833a1517619dd67d2ebb8acf6e
justanotherdevel <shashwatyashaswi@gmail.com>
2017-04-25 02:30:48 +0530

    Finished up dijkstra

--- maze.js
+++ maze.js
@@ -1,10 +1,9 @@
 //Creating a class for storing maze.
 var Maze = (function () {
-    function Maze(r, c) {
+    function Maze(Vertices) {
         //Fill this with whatever values you find suitable for connections in maze.
         this.connectionPos = [[1, 3], [3, 5]];
-        this.row = r;
-        this.column = c;
+        this.nVertices = Vertices;
         var noConnection = Math.floor(Math.random() * (this.maxConnection - this.minConnection)) + this.minConnection;
         for (var i = 0; i < noConnection; i++) {
             var pos = Math.floor(Math.random() * this.noPos);
@@ -14,14 +13,49 @@ var Maze = (function () {
     Maze.prototype.extractWeight = function (i, j) {
         return this.maze[i][j];
     };
-    Maze.prototype.dijkstra = function (source, destination) {
+    Maze.prototype.minDistance = function (dist, sptSet) {
+        var min = Infinity;
+        var mIndex;
+        for (var i = 0; i < this.nVertices; i++) {
+            if (sptSet[i] == false && dist[i] <= min) {
+                min = dist[i];
+                mIndex = i;
+            }
+        }
+        return mIndex;
+    };
+    Maze.prototype.dijkstra = function (src, path) {
         //Apply dijkstra and check if destination is reachable.
         //Return true or false accordingly.
+        var dist;
+        var sptSet;
+        for (var i = 0; i < this.nVertices; i++) {
+            dist[i] = Infinity;
+            sptSet[i] = false;
+        }
+        dist[src] = 0;
+        for (var i = 0; i < this.nVertices - 1; i++) {
+            //Takes the minimum distance vertex from the set of vertices that is yet to be processed
+            var u = this.minDistance(dist, sptSet);
+            //Marks the just explored vertex as processed
+            sptSet[u] = true;
+            for (var v = 0; v < this.nVertices; v++) {
+                //Update dist[v] only if it is not in sptSet,
+                //there is an edge from u to v,
+                //and total weight of path from source to v through u is
+                //smaller than the current value of dist[v]
+                if (!sptSet[v] && this.maze[u][v] && dist[u] != Infinity && dist[u] + this.maze[u][v] < dist[v])
+                    dist[v] = dist[u] + this.maze[u][v];
+                path[v] = u;
+            }
+            path[src] = -1;
+        }
     };
     return Maze;
 }());
 var maze;
-maze = new Maze(50, 50);
-while (!maze.dijkstra(0, 0, 49, 49)) {
-    maze = new Maze(50, 50);
+maze = new Maze(50);
+var path;
+while (!maze.dijkstra(0, path)) {
+    maze = new Maze(50);
 }
